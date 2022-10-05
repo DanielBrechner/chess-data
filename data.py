26ea07939eafac4bf5e1b1da690ebbd90c623fd7
danielbrechner <brechner.daniel@gmail.com>
2022-10-05 05:15:01 -0400

    venv

--- data.py
+++ data.py
@@ -1,7 +1,9 @@
-from secrets.py import api_key
-import numpy as np
-import matplotlib.pyplot as plt
-import berserk
-pip install berserk
-session = berserk.TokenSession('api_key')
-client = berserk.Client(session=session)
+#import berserk
+#import matplotlib.pyplot as plt
+#import numpy as np
+#from secrets.py import api_key
+import sys
+print(sys.path)
+# pip install berserk
+#session = berserk.TokenSession('api_key')
+#client = berserk.Client(session=session)
